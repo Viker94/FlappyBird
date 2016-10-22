7b0da499fec0e0dc6bd0db50cde5026515c77751
Ciesiu <fisher94.poczta@gmail.com>
2016-10-22 14:46:50 +0200

    Poprawka losowości generowania przeszkód

--- Assets/Script/Obstacle.cs
+++ Assets/Script/Obstacle.cs
@@ -6,16 +6,16 @@ public class Obstacle : MonoBehaviour {
     // Use this for initialization
 
     public Vector2 velocity = new Vector2(-4, 0);
-    public float range = 0.5F;
+    //public float range = 4F;
     bool wasVisible = false;
 
 	void Start () {
         Rigidbody2D rg = GetComponent<Rigidbody2D>();
         rg.velocity = velocity;
-        float random = transform.position.y - range * Random.value;
+        float random = transform.position.y - Random.Range(-2.0F,2.0F);
         while (random < -1)
         {
-            random = transform.position.y - range * Random.value;
+            random = transform.position.y - Random.Range(-2.0F,2.0F);
         }
         transform.position = new Vector3(transform.position.x, random, transform.position.z);
     }
