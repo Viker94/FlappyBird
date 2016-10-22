c22d19f44ec3aa618a456632e883f991a3aa1966
Viker94 <mfilipek94@gmail.com>
2016-10-22 18:37:40 +0200

    Zmiana końca gry, dodany Highscore, dodany exe do projektu

--- Assets/Script/EndGame.cs
+++ Assets/Script/EndGame.cs
@@ -4,22 +4,41 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class EndGame : MonoBehaviour {
-	
+
+    int highscore = 0;
+
+    void Start()
+    {
+        highscore = PlayerPrefs.GetInt("HighScore");
+    }
+
 	// Update is called once per frame
 	void Update () {
         if (Time.timeScale == 0)
         {
-            GameObject.Find("Score").GetComponent<Text>().text = "Score: " + PlayerMovement.score;
+            GameObject.Find("Score").GetComponent<Text>().text = "Score: " + PlayerMovement.score; if (PlayerMovement.score > highscore)
+            {
+                highscore = PlayerMovement.score;
+                PlayerPrefs.SetInt("HighScore", highscore);
+            }
+            GameObject.Find("Highscore").GetComponent<Text>().text = "Highscore: " + highscore;
             GameObject.Find("ScorePlay").GetComponent<Text>().text = "";
-            GameObject.Find("Restart").GetComponent<Text>().text = "Press \"R\" to restart";
+            GameObject.Find("Restart").GetComponent<Text>().text = "Press \"R\" to restart\nPress \"ESC\" to exit";
             if (Input.GetKeyUp("r")){
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
                 Time.timeScale = 1;
                 PlayerMovement.score = 0;
             }
+            if (Input.GetKeyUp("escape"))
+            {
+                Application.Quit();
+            }
         }
         else
         {
+            GameObject.Find("Score").GetComponent<Text>().text = "";
+            GameObject.Find("Restart").GetComponent<Text>().text = "";
+            GameObject.Find("Highscore").GetComponent<Text>().text = "";
             GameObject.Find("ScorePlay").GetComponent<Text>().text = PlayerMovement.score + "";
         }
 	}
