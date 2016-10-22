6986772b360b14ca413fd14b7dc716230c1ee01e
Viker94 <mfilipek94@gmail.com>
2016-10-22 09:24:59 +0200

    First comit

---
+++ Assets/Script/BeeMove.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeeMove : MonoBehaviour
+{
+
+    Vector3 velocity = Vector3.zero;
+    float flapSpeed = 300f;
+    float forwardSpeed = 0.5f;
+    float maxSpeed = 2f;
+    bool Flap = false;
+    bool dead = false;
+    bool dzwiek = false;
+    AudioSource saw;
+    float delay = 0;
+    Animator animator;
+    // Use this for initialization
+    void Start()
+    {
+        animator = transform.GetComponentInChildren<Animator>();
+        saw = GameObject.FindObjectOfType<AudioSource>();
+
+    }
+
+
+    /// Grafika 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Flap = true;
+
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel(0);
+        }
+    }
+
+
+    // Fizyka
+    void FixedUpdate()
+    {
+        if (dead == false)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right * forwardSpeed);
+
+            if (GetComponent<Rigidbody2D>().velocity.x > maxSpeed)
+
+            {
+
+                GetComponent<Rigidbody2D>().velocity = new Vector2(maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            }
+            if (Flap)
+            {
+                GetComponent<Rigidbody2D>().AddForce(Vector2.up * flapSpeed);
+                Flap = false;
+            }
+
+            if (GetComponent<Rigidbody2D>().velocity.y >= 0.25)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 1);
+            }
+            else
+            {
+                float angle = Mathf.Lerp(0, -30, -GetComponent<Rigidbody2D>().velocity.y / 2f);
+                transform.rotation = Quaternion.Euler(0, 0, angle);
+
+            }
+        }
+        else
+        {
+            Debug.Log("dasdaA");
+            
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -2);
+
+
+
+
+
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (dzwiek == false)
+        {
+            saw.Play();
+            dzwiek = true;
+        }
+        animator.SetTrigger("smierc");
+        Debug.Log("AAAAAAAAAAAA");
+        dead = true;
+        if (coll.gameObject.tag == "ziemia" || coll.gameObject.tag == "miodekdol")
+        {
+            GetComponent<Rigidbody2D>().MoveRotation(180);
+        }
+    }
+}
---
+++ Assets/Script/TloLooper.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TloLooper : MonoBehaviour
+{
+    int panels = 9;
+    float random;
+    int type = 0;
+    bool para = false;
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        if (collider.gameObject.tag == "miodek")
+        {
+            Vector3 poss = collider.transform.position;
+            poss.x += 33;
+            random=Random.Range(-0.5f, 0.5f);
+            Debug.Log(random + " " + poss.y + " " + collider.name);
+            if (poss.y > 3.8)
+            {
+                poss.y -= 0.3f;
+                type = 0;
+            }
+            else if (poss.y < 2.4)
+            {
+                poss.y += 0.3f;
+                type = 1;
+            }
+
+            else
+            {
+                
+                poss.y += random;
+                type = 2;
+            }
+            collider.transform.position = poss;
+
+        }else if (collider.gameObject.tag == "miodekdol")
+            {
+            Vector3 poss = collider.transform.position;
+            poss.x += 33;
+            if (type == 0) poss.y -= 0.3f;
+            else if (type == 1) poss.y += 0.3f;
+            else poss.y += random;
+            collider.transform.position = poss;
+        }
+        else
+        {
+            float widthofBGObject = ((BoxCollider2D)collider).size.x;
+            Vector3 pos = collider.transform.position;
+            pos.x += widthofBGObject * panels - widthofBGObject / 2 - 1;
+            collider.transform.position = pos;
+        }
+    }
+}
