afdbe0ed797390c935632d98b36f35505aa3ead2
Norbert Hellwig <norhel93@gmailcom>
2016-10-25 11:46:42 +0200

    Dodane menu startowe razem ze znikaniem, nie wiem czemu odpala gre od razu nie czekajac na jakikolwiek nacisniety przycisk, wczesniej tego nie bylo

--- Assets/Script/EndGame.cs
+++ Assets/Script/EndGame.cs
@@ -3,7 +3,8 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class EndGame : MonoBehaviour {
+public class EndGame : MonoBehaviour
+{
 
     int highscore = 0;
 
@@ -12,8 +13,9 @@ public class EndGame : MonoBehaviour {
         highscore = PlayerPrefs.GetInt("HighScore");
     }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         if (Time.timeScale == 0)
         {
             GameObject.Find("Score").GetComponent<Text>().text = "Score: " + PlayerMovement.score; if (PlayerMovement.score > highscore)
@@ -24,7 +26,8 @@ public class EndGame : MonoBehaviour {
             GameObject.Find("Highscore").GetComponent<Text>().text = "Highscore: " + highscore;
             GameObject.Find("ScorePlay").GetComponent<Text>().text = "";
             GameObject.Find("Restart").GetComponent<Text>().text = "Press \"R\" to restart\nPress \"ESC\" to exit";
-            if (Input.GetKeyUp("r")){
+            if (Input.GetKeyUp("r"))
+            {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
                 Time.timeScale = 1;
                 PlayerMovement.score = 0;
@@ -36,10 +39,15 @@ public class EndGame : MonoBehaviour {
         }
         else
         {
+            if (Input.GetKeyUp("space"))
+            {
+                GameObject.Find("Start").GetComponent<Text>().text = "";
+                Destroy(GameObject.Find("menu-PSD"));
+            }
             GameObject.Find("Score").GetComponent<Text>().text = "";
             GameObject.Find("Restart").GetComponent<Text>().text = "";
             GameObject.Find("Highscore").GetComponent<Text>().text = "";
             GameObject.Find("ScorePlay").GetComponent<Text>().text = PlayerMovement.score + "";
         }
-	}
+    }
 }
